461d3327f82502549f5c5ce182f8fe2d45b44a85
lambnoah99 <n.lambert2@web.de>
2023-09-26 20:37:08 +0200

    Add Static File Server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Static Files
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api", require("./routes/api"));
