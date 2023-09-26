8f038831a075bfa172c1699e5136874ede3b679e
lambnoah99 <n.lambert2@web.de>
2023-09-26 20:34:40 +0200

    Add Middleware

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // Routes
 app.use("/api", require("./routes/api"));
